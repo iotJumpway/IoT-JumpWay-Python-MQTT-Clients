d5d62d7ba930f1181b2c3c001b6bea641a72c38b
AdamMiltonBarker <adammiltonbarker@gmail.com>
2016-12-01 23:49:59 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+# Setup script for installing techbubbleiotjumpwaymqtt#
+# Author:   Adam Milton-Barker <adammiltonbarker@gmail.com>#
+# Copyright (C) 2016 - 2017 TechBubble Technologies Limited
+# For license information, see LICENSE.txt
+
 import sys
 sys.path.insert(0, 'src')
 
@@ -8,17 +13,20 @@ except ImportError:
 
 setup(
     name='techbubbleiotjumpwaymqtt',
-    version="0.0.1",
+    version="0.2.0",
     author='Adam Milton-Barker',
     author_email='adammiltonbarker@gmail.com',
     package_dir={'': 'src'},
 	package_data={'techbubbleiotjumpwaymqtt': ['*.pem']},
     url='https://github.com/TechBubbleTechnologies/TechBubble-Iot-JumpWay-Python-MQTT',
-    license='MIT',
+    license='Eclipse Public License v1.0',
     description='TechBubble Technologies IoT JumpWay MQTT Client for Python',
     packages=['techbubbleiotjumpwaymqtt'],
     install_requires=[
         "paho-mqtt >= 1.1",
+    ],
+    keyword=[
+        'techbubbleiotjumpwaymqtt', 'python', 'setup', 'pypi'
     ],
     classifiers=[]
 )
